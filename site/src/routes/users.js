5f5b8b721b0524baeb653f5c356bffa62d39729f
DiegoBrusco <diegoabrusco@gmail.com>
2020-06-24 14:28:40 -0300

    auth

--- site/src/routes/users.js
+++ site/src/routes/users.js
@@ -3,19 +3,20 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 const validator = require('../middlewares/validator');
 const guestMiddleware = require('../middlewares/guest');
+const authtMiddleware = require('../middlewares/auth');
 
 //ROUTES
 // /users/register
 router.get('/register', guestMiddleware, usersController.register);
-router.post('/register', validator.register, usersController.processRegister);
+router.post('/register', validator.register, guestMiddleware,  usersController.processRegister);
 
 
 // /users/login
 router.get('/login', guestMiddleware, usersController.login); //si el usuario que está entrando al sitio, esta en session, lo redirigimos al home para que empiece a navegar. sino, lo lleva al login
-router.post('/login', usersController.processLogin); //aca tenemos que poner el validator.login
+router.post('/login', guestMiddleware, usersController.processLogin); //aca tenemos que poner el validator.login
 
 //logout
-router.post('/logout', usersController.logout);
+router.post('/logout', authtMiddleware, usersController.logout);
 
 //EXPORTS
 module.exports = router;
