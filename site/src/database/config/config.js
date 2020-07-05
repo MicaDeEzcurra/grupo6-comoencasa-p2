57482996b0758e77a5eadee8b20e6bc632ce44dd
Clara Benadiba <clarabenadiba@gmail.com>
2020-07-05 16:34:49 -0300

    cerrar sesion y crear producto dentro del perfil

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,11 +1,12 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "comoencasa",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": 0,
+    "port": 8889,
       "define": {
       "paranoid": true
     }
