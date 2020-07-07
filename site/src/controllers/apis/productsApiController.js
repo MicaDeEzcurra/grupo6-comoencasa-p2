2d3af3c88f83efae5fd08a7f84bb6a68a9919439
Mica de Ezcurra <micaeladezcurra@gmail.com>
2020-07-07 15:52:12 -0300

    api product detail

--- site/src/controllers/apis/productsApiController.js
+++ site/src/controllers/apis/productsApiController.js
@@ -1,4 +1,4 @@
-const { Product } = require('../../database/models');
+const { Product , Category } = require('../../database/models');
 
 const productsApiController = {
   
@@ -39,7 +39,51 @@ const productsApiController = {
 
             return res.json(apiResponse)
         });
-    }
+    },
+
+    detail: (req, res) => {
+
+        Product.findByPk(req.params.productId, {
+                include: ['category', 'user']
+            })
+            .then((product) => {
+
+                let apiResponse = null;
+    
+                if(product){
+                    apiResponse = {
+                        meta: {
+                            status: 200,
+                            length: product.length,
+                            url: req.originalUrl
+                        },
+                        data: product
+                    }
+    
+                } else{
+                    apiResponse = {
+                        meta: {
+                            status: 404
+                        },
+                        data: null
+                    }    
+                };
+    
+                return res.json(apiResponse)
+            })
+            .catch(error => {
+                let apiResponse = {
+                    meta: {
+                        status: 404
+                    },
+                    data: null
+                }
+    
+                return res.json(apiResponse)
+            });
+
+    },
+
 };
 
 module.exports = productsApiController;
--- site/src/routes/apis/productsApiRouter.js
+++ site/src/routes/apis/productsApiRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const productsApiController = require('../../controllers/apis/productsApiController');
 
 router.get('/', productsApiController.index);
+router.get('/detail/:productId/', productsApiController.detail);
+
+
 
 
 module.exports = router;
