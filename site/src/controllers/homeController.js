3edbac4bb6b3b0e37d4e3e76bc2197ed71d6c879
DiegoBrusco <diegoabrusco@gmail.com>
2020-07-11 09:20:40 -0300

    links del footer

--- site/src/controllers/homeController.js
+++ site/src/controllers/homeController.js
@@ -4,7 +4,36 @@ const homeController = {
             title: 'HOME'
         }
         return res.render('home', vac);
-    }
+    },
+
+    cookies: (req, res) => {
+        let vac = {
+            title: 'COOKIES'
+        }
+        return res.render('cookies', vac);
+    },
+
+    envios: (req, res) => {
+        let vac = {
+            title: 'ENVIOS'
+        }
+        return res.render('envios', vac);
+    },
+
+    privacidad: (req, res) => {
+        let vac = {
+            title: 'PRIVACIDAD'
+        }
+        return res.render('privacidad', vac);
+    },
+
+    contacto: (req, res) => {
+        let vac = {
+            title: 'CONTACTO'
+        }
+        return res.render('contacto', vac);
+    },
+
 };
 
 module.exports = homeController;
--- site/src/routes/home.js
+++ site/src/routes/home.js
@@ -5,6 +5,13 @@ const homeController = require('../controllers/homeController');
 /* GET home page. */
 router.get('/', homeController.index);
 
+router.get('/privacidad', homeController.privacidad);
+router.get('/cookies', homeController.cookies);
+router.get('/envios', homeController.envios);
+router.get('/contacto', homeController.contacto);
+
+
+
 module.exports = router;
 
 //TERMINADO
