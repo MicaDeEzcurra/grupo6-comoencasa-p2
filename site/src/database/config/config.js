e1c0509d4cf650d4953a3de2a84cdb2fb650cfb8
leanmarelli <lmarelli17@gmail.com>
2020-07-24 18:20:12 -0300

    ultimos detalles

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "comoencasa",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 8889,
     "operatorsAliases": 0,
       "define": {
       "paranoid": true
