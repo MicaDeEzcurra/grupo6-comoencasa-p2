88d4d89e6ae4b2f15b26bad02273f525d6ac4a16
Clara Benadiba <clarabenadiba@gmail.com>
2020-07-25 15:00:12 -0300

    Cambios

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "comoencasa",
     "host": "127.0.0.1",
+    "port": 8889,
     "dialect": "mysql",
     "operatorsAliases": 0,
       "define": {
